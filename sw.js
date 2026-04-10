0672a9749908732de2d04b7024c6b9465dcd7784
chrissspp <christianjprince12@gmail.com>
2026-04-09 23:36:26 -0400

    Refactor fetch event listener in service worker
    Updated fetch event listener to simplify async handling.

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
-importScripts('./scramjet.all.js');
+// This pulls the engine from the web so you don't need the file
+importScripts('https://cdn.jsdelivr.net/gh/MercuryWorkshop/scramjet@main/dist/scramjet.all.js');
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(async () => {
-        // This line is the magic that unblocks sites
-        await scramjet.loadConfig();
-        if (scramjet.route(event)) {
-            return scramjet.fetch(event);
-        }
-        return fetch(event.request);
-    });
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        (async () => {
+            if (scramjet.route(event)) {
+                return scramjet.fetch(event);
+            }
+            return fetch(event.request);
+        })()
+    );
 });

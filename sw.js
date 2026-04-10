afbf22f2a25afc3b5029fd84fa039aa41e126e87
chrissspp <christianjprince12@gmail.com>
2026-04-09 23:42:20 -0400

    Update service worker to use new Scramjet imports

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
-// This pulls the engine from the web so you don't need the file
-importScripts('https://cdn.jsdelivr.net/gh/MercuryWorkshop/scramjet@main/dist/scramjet.all.js');
+importScripts('https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@latest/dist/scramjet.codecs.js');
+importScripts('https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@latest/dist/scramjet.worker.js');
 
-const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        (async () => {
-            if (scramjet.route(event)) {
-                return scramjet.fetch(event);
-            }
-            return fetch(event.request);
-        })()
-    );
+    if (event.request.url.startsWith(self.location.origin + '/service/')) {
+        event.respondWith(scramjet.fetch(event));
+    }
 });

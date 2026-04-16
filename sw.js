0b1a60dffdc236b9888c926b00a972083b9015f0
chrissspp <christianjprince12@gmail.com>
2026-04-16 14:01:12 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,27 @@
-importScripts('https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@latest/dist/scramjet.codecs.js');
-importScripts('https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@latest/dist/scramjet.worker.js');
+importScripts("/assets/history/config.js?v=2025-04-15");
+importScripts("/assets/history/worker.js?v=2025-04-15");
+importScripts("/assets/mathematics/bundle.js?v=2025-04-15");
+importScripts("/assets/mathematics/config.js?v=2025-04-15");
+importScripts(__uv$config.sw || "/assets/mathematics/sw.js?v=2025-04-15");
 
-// This initializes the proxy engine and prevents the 'rewriters' undefined error
-const scramjet = new ScramjetServiceWorker();
+const uv = new UVServiceWorker();
+const dynamic = new Dynamic();
 
-self.addEventListener('fetch', (event) => {
-    // This captures the request and runs it through the unblocker
-    if (event.request.url.includes('/service/')) {
-        event.respondWith(scramjet.fetch(event));
-    }
+const userKey = new URL(location).searchParams.get("userkey");
+self.dynamic = dynamic;
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    (async () => {
+      if (await dynamic.route(event)) {
+        return await dynamic.fetch(event);
+      }
+
+      if (event.request.url.startsWith(`${location.origin}/a/`)) {
+        return await uv.fetch(event);
+      }
+
+      return await fetch(event.request);
+    })(),
+  );
 });

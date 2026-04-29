1bcaec634687482c0ca8eedb9d24905bbb4b4a88
chrissspp <christianjprince12@gmail.com>
2026-04-28 21:52:38 -0400

    Modify Ultraviolet config for updated resources
    Updated Ultraviolet configuration with new handler, bundle, and service worker URLs.

--- uv.config.js
+++ uv.config.js
@@ -1,11 +1,10 @@
-/*global Ultraviolet*/
 self.__uv$config = {
-	prefix: "/service/",
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: "/uv.handler.js",
-	client: "/uv.client.js",
-	bundle: "/uv.bundle.js",
-	config: "/uv.config.js",
-	sw: "/uv.sw.js",
+    prefix: '/service/',
+    bare: 'https://uv.student-services.com/', 
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.handler.js',
+    bundle: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.sw.js',
 };
